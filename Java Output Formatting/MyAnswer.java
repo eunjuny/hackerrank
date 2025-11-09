940a048c55ae02d78f61d7cdddb5024178930a2e
Yeop Eunjun <105467796+eunjuny@users.noreply.github.com>
2025-11-10 00:00:41 +0900

    Update MyAnswer.java

--- Java Output Formatting/MyAnswer.java
+++ Java Output Formatting/MyAnswer.java
@@ -15,21 +15,10 @@ public class Solution {
     }
     
     private static String setOutput(String str) {
-        String one = str.split(" ")[0];
-        String two = str.split(" ")[1];
-        
-        int i = one.length();
-        int j = Integer.parseInt(two);
-        while(15 > i) {
-           one = one + " ";
-           i++;
-        }
-        if (j < 10) {
-            two = "00" + two;
-        } else if (j < 100) {
-            two = "0" + two;
-        }
-        
-        return one + two;
+        String[] parts = str.trim().split("\\s+");
+        String word = parts[0];
+        int num = Integer.parseInt(parts[1]);
+        return String.format("%-15s%03d", word, num);
     }
+
 }
